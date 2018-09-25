d03b624bf6eb835f8dfbfd9d398db6735917e4cb
Fernando Pereira de Castro <c1281432@bb.com.br>
2018-09-25 16:43:09 -0300

    alterações

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,20 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import {AngularFireModule} from 'angularfire2'
+import {AngularFireDatabaseModule} from 'angularfire2/database'
 
 import { MyApp } from './app.component';
+import { FIREBASE_CONFIG } from './firebase.credentials';
 
 
 @NgModule({
   declarations: [MyApp],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(FIREBASE_CONFIG),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: "AIzaSyCrngRsG-BxOiP4WEm5WLpnBa6myeUSZKE",
+    authDomain: "af2lists-3057a.firebaseapp.com",
+    databaseURL: "https://af2lists-3057a.firebaseio.com",
+    projectId: "af2lists-3057a",
+    storageBucket: "af2lists-3057a.appspot.com",
+    messagingSenderId: "349921918460"
+};
